be2cc4e02b9150e5cead435bd55e1ffd82d12142
Rudy <rudydimacali@gmail.com>
2019-01-30 11:15:27 -0800

    Implement react-paginate with results table.

--- challenge_1/client/src/components/App.jsx
+++ challenge_1/client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchResults from './SearchResults.jsx';
+import ReactPaginate from 'react-paginate';
 const axios = require('axios');
 
 export default class App extends React.Component {
@@ -10,10 +11,15 @@ export default class App extends React.Component {
         date: 'Loading...',
         description: '',
       }],
+      data: [],
+      offset: 0,
+      pageCount: 1
     };
     this.getResults = this.getResults.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
+    this.changePage = this.changePage.bind(this);
   }
 
   componentDidMount() {
@@ -24,8 +30,9 @@ export default class App extends React.Component {
     axios.get(`http://localhost:3000/events?q=${queue}`)
       .then((response) => {
         this.setState({
-          events: response.data
-        });
+          events: response.data,
+          pageCount: response.data.length / 10
+        }, this.changePage);
       });
   }
 
@@ -40,32 +47,45 @@ export default class App extends React.Component {
     this.getResults(this.state.query);
   }
 
+  changePage() {
+    let data = this.state.events.slice(this.state.offset, this.state.offset + 10);
+    console.log(data);
+    this.setState({ data });
+  }
+
+  handlePageClick(data) {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * 10);
+    console.log(selected);
+    this.setState({ offset: offset }, this.changePage());
+  };
+
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a class="navbar-brand" href="#">Historical Events Finder</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand" href="#">Historical Events Finder</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#"><span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#"></a>
               </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="text" placeholder="Search" id='query' onChange={this.handleChange} />
-              <button class="btn btn-secondary my-2 my-sm-0" type="submit" onClick={this.handleSubmit}>Search</button>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" id='query' onChange={this.handleChange} />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={this.handleSubmit}>Search</button>
             </form>
           </div>
         </nav>
@@ -75,9 +95,22 @@ export default class App extends React.Component {
               <th>Date</th>
               <th>Description</th>
             </tr>
-            <SearchResults results={this.state.events} />
+            <SearchResults results={this.state.data} />
           </tbody>
         </table>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
       </div>
     )
   }
