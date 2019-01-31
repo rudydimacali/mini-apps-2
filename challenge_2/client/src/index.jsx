702147df929c5a1b74a454abc7cfea2118f166df
Rudy <rudydimacali@gmail.com>
2019-01-30 19:53:39 -0800

    Remove unused app.jsx.

--- challenge_2/client/src/index.jsx
+++ challenge_2/client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
+import Chart from './components/chart.jsx';
 import 'bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Chart />, document.getElementById('app'));
